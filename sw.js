e0b0070e33e3a4d4e8a4a7dee02d91414fc7e149
aibotvivi-github <aibotvivi@gmail.com>
2026-05-16 09:50:27 +0100

    SplitEasy app

---
+++ sw.js
@@ -0,0 +1,33 @@
+const CACHE = 'spliteasy-v1';
+const SHELL = ['/', '/index.html', '/manifest.json', '/icon.svg'];
+
+self.addEventListener('install', e => {
+  e.waitUntil(caches.open(CACHE).then(c => c.addAll(SHELL)));
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  // Network-first for API calls, cache-first for app shell
+  const url = new URL(e.request.url);
+  const isAPI = url.hostname !== self.location.hostname;
+  if (isAPI) {
+    e.respondWith(fetch(e.request).catch(() => new Response('', { status: 503 })));
+  } else {
+    e.respondWith(
+      caches.match(e.request).then(r => r || fetch(e.request).then(res => {
+        const clone = res.clone();
+        caches.open(CACHE).then(c => c.put(e.request, clone));
+        return res;
+      }))
+    );
+  }
+});
